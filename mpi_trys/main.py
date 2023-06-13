dc8457fb340d6e71112a5fabafe75d07c6218b9f
Rafay <rafayalvi17@gmail.com>
2023-06-13 19:38:57 +0200

    mpi implementations done separately with small programs

---
+++ mpi_trys/main.py
@@ -0,0 +1,28 @@
+
+from dist_mpi import *
+
+# sayHello()
+
+N = 32
+L = 8
+h = 0.2
+
+
+D = lambda x : 1
+S = lambda x,t : 0
+
+# eachproc(N)
+Determine_Value(L,h,D)
+# shape = 4
+
+# local_mat = make_mat()
+# print(f"matrix at {rank}")
+# print(local_mat)
+# print()
+
+
+# swap_mat = exchange_vals(local_mat)
+# print(swap_mat)
+# print()
+
+
